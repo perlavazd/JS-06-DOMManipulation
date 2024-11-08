1fe8e0cca1884378a12a7ca04dd028997dcf0816
perlavazd <perlavazd@gmail.com>
2024-11-08 07:01:58 -0600

    Primera parte ejercicio

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+const users = [
+    {
+        id: 1,
+        user_name: 'User1',
+        description: 'lorem ipsum',
+        age: '46',
+        fav_music: {
+            bands: [
+                'Band 1', 'Band 2', 'Band 3', 'Band 4'
+            ]
+        }
+    },
+    {
+        id: 2,
+        user_name: 'User LastName',
+        description: 'Lorem lorem lorem',
+        age: '23',
+        fav_music: {
+            bands: [
+               'Band 1', 'Band 2', 'Band 3', 'Band 4'
+            ]
+        }
+    }
+]
+
+//Aquí optengo un array de cada obejto
+const namee = users.map(user => user.user_name);
+const descr = users.map(user => user.description);
+const ageUser = users.map(user => user.age);
+const bandList = users.map(user => user.fav_music.bands);
+
+
+//Hice un contador considerando longitud del array de usuario, para que posteriormente ciclo for me formara la cantidad de tarjetas acorde a la cantidad de usuarios
+let count=0;
+for (let i=0; i < namee.length; i++){
+    count++;   
+}
+
+//Utilice una funcion que tuviera como valor el contador de usuarios
+function componenteCard (count){
+
+    const contenedor = document.getElementById("contenedorCards"); //En este contenedor de mi html se agregaran las nuevas tarjetas hijas
+
+    for (let i = 0; i < count; i++) { //Se realiza ciclo for para crear tarjetas con inf
+        const nameInCard = document.createElement("div");  //Se crea div
+        nameInCard.classList.add("card");  //Se le añade una clase al div
+
+        const pName = document.createElement("p"); //Se crea un parrafo al cual se le agrega como contenido el valor del objeto.
+        pName.textContent = `Nombre: ${namee[i]}`;
+        nameInCard.appendChild(pName); //Se agrega elemento a la tarjeta div creada.
+
+        const pdescr = document.createElement("p");
+        pdescr.textContent = `Descripción: ${descr[i]}`;
+        nameInCard.appendChild(pdescr);
+
+        const pAgeUser = document.createElement("p");
+        pAgeUser.textContent = `Edad: ${ageUser[i]}`;
+        nameInCard.appendChild(pAgeUser);
+
+       
+
+
+
+        
+        contenedor.appendChild(nameInCard); //Se agrega todos los elementos al div ppadre
+    }
+}
+
+
+componenteCard(count); //Se manda llamar la función y como parametro se le asigna el contador
